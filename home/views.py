4a961bc1b4f04a75e08c54a84685fcbd5889dff9
benup211 <benup211@gmail.com>
2024-07-23 21:12:07 +0545

    team endpoint,event and blog in homescreen

--- home/views.py
+++ home/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.views import View
-
-
+from event.models import GrandEventSystem,EventSystem
+from blog.models import AWSBlog
 class HomeView(View):
 
     def get(self,request):
-        return render(request,'home/homepage.html')
+        latest_events = EventSystem.objects.order_by('-startDate')[:3]
+        latest_blogs=AWSBlog.objects.order_by('-date_of_publish')[:3]
+        context={
+            'events':latest_events,
+            'blogs':latest_blogs
+        }
+        return render(request,'home/homepage.html',context)
