f7d681b147dc9b9b0988b31e8e6cb9a35723bd07
benup211 <benup211@gmail.com>
2024-07-26 19:06:20 +0545

    Added GrandEvent data

--- home/views.py
+++ home/views.py
@@ -2,14 +2,18 @@ from django.shortcuts import render
 from django.views import View
 from event.models import GrandEventSystem,EventSystem
 from blog.models import AWSBlog
+from django.utils import timezone
 class HomeView(View):
 
     def get(self,request):
         latest_events = EventSystem.objects.order_by('-startDate')[:3]
         latest_blogs=AWSBlog.objects.order_by('-date_of_publish')[:3]
+        current_time = timezone.now()
+        grand_event = GrandEventSystem.objects.filter(EventStatus='Ongoing',startDate__gt=current_time).order_by('startDate')[0]
         context={
             'events':latest_events,
-            'blogs':latest_blogs
+            'blogs':latest_blogs,
+            'grand_event':grand_event
         }
         return render(request,'home/homepage.html',context)
     
