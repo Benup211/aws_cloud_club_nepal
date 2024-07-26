c89e544798ba8b91b59a1699af8831907bb550db
benup211 <benup211@gmail.com>
2024-07-26 19:09:11 +0545

    solved out of index error

--- home/views.py
+++ home/views.py
@@ -9,7 +9,7 @@ class HomeView(View):
         latest_events = EventSystem.objects.order_by('-startDate')[:3]
         latest_blogs=AWSBlog.objects.order_by('-date_of_publish')[:3]
         current_time = timezone.now()
-        grand_event = GrandEventSystem.objects.filter(EventStatus='Ongoing',startDate__gt=current_time).order_by('startDate')[0]
+        grand_event = GrandEventSystem.objects.filter(EventStatus='Ongoing',startDate__gt=current_time).order_by('startDate').first()
         context={
             'events':latest_events,
             'blogs':latest_blogs,
