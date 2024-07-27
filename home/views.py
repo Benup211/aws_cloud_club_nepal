84134a6cb52c993571946ea8487ba9b1b2f40dc9
benup211 <benup211@gmail.com>
2024-07-27 08:36:23 +0545

    error fix for grandevent query

--- home/views.py
+++ home/views.py
@@ -9,6 +9,7 @@ class HomeView(View):
         latest_events = EventSystem.objects.order_by('-startDate')[:3]
         latest_blogs=AWSBlog.objects.order_by('-date_of_publish')[:3]
         current_time = timezone.now()
+        current_time=current_time.replace(hour=0, minute=0, second=0)
         grand_event = GrandEventSystem.objects.filter(EventStatus='Ongoing',startDate__gt=current_time).order_by('startDate').first()
         context={
             'events':latest_events,
