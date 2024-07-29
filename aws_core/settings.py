feafdaaafe939c3a3f854f94944f285a447e4744
rajendraacharya1ls <rajendraacharya1ls@gmail.com>
2024-07-29 11:00:29 +0545

    minor changes

--- aws_core/settings.py
+++ aws_core/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0r&xutc=y4d)aey0eyd$rg=m5k#%rwp*_!r4$9=@!r1p&3az*1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
